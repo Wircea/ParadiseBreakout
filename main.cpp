24f18f7f68c9a6439d49207d696411f75526f7a5
Wircea <wircea@yahoo.com>
2018-05-10 17:36:28 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* args[])
+{
+    return 0;
+}
